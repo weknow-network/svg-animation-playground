a49ed48c4b721fd8a91b0ab9f062332b28b66443
bnaya eshet <bnayanet@hotmail.com>
2020-03-19 12:04:10 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 // import logo from './logo.svg';
-import './App.css';
-import { ReactComponent as OneLine } from './sample.svg';
+import "./App.css";
+import { ReactComponent as OneLine } from "./sample.svg";
 
 function App() {
   // var path = document.querySelector('.path');
   // var length = path.getTotalLength();
   // console.log(length);
-  
+
   return (
     <>
-      <OneLine width={1500} height={1500}/>
+      <OneLine width={2081} height={793} />
     </>
   );
 }
